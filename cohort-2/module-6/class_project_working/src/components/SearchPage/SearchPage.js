e3bbc41456283d214abb5f14d232a991439f4e9d
Dominique Wynn <80863089+InkiiSheepii@users.noreply.github.com>
2021-06-15 17:29:17 -0400

    commitPages3

--- cohort-2/module-6/class_project_working/src/components/SearchPage/SearchPage.js
+++ cohort-2/module-6/class_project_working/src/components/SearchPage/SearchPage.js
@@ -2,15 +2,16 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 
 import { SearchGrid } from "./SearchGrid"
+import { SearchInput } from "./SearchInput"
 
 export const SearchPage = (props) => {
   const [items, setItems] = useState(null)
-
+  const [query, setQuery] = useState("")
   useEffect(() => {
     axios
       .get("/api/product-search", {
         params: {
-          query: "",
+          query,
           filterBy: "",
           sortBy: "",
         },
@@ -18,11 +19,24 @@ export const SearchPage = (props) => {
       .then((response) => {
         setItems(response.data.items)
       })
-  }, [])
-
+  }, [query])
   return (
-    <div>
-      <div>Search Input</div>
+    <div className="SearchPage">
+      <div className="row pl-4 pl-md-0 pr-4 pr-md-0">
+        <div className="d-none d-md-block col-md-3" />
+        <div className="col-md-6">
+          <form
+            onSubmit={(event) => {
+              event.preventDefault()
+              const searchInputValue =
+                event.target.querySelector("input[name=query]").value
+              setQuery(searchInputValue)
+            }}
+          >
+            <SearchInput name="query" />
+          </form>
+        </div>
+      </div>
       <div>Search Options</div>
       <SearchGrid items={items} />
     </div>
